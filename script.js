e52e1813041b1417b54028d00e520519d7cde9ea
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-07-21 00:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+let strongElements =document.querySelectorAll("strong");
 function highlight() {
     //Write your code here
-
-
+	for (let i = 0; i < strongElements.length; i++) {
+    strongElements[i].style.color = "rgb(0, 128, 0)";
+  }
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+for (let i = 0; i < strongElements.length; i++) {
+    strongElements[i].style.color = "rgb(0, 0, 0)";
+  }
 }
